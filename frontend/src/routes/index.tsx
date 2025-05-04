dfae857ed6f86da939388b783cdbe6ddce277bc9
Atreya <44151328+atreya2011@users.noreply.github.com>
2025-05-05 08:18:57 +0900

    fix: update frontend paths and dependencies for connect-query integration

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,13 @@
+import { listInstruments } from "@/gen/client/expenses/v1/instrument-InstrumentService_connectquery"
 import { useQuery } from "@connectrpc/connect-query"
 import { createFileRoute } from "@tanstack/react-router"
-import { listUsers } from "api/client/expenses/v1/user-UserService_connectquery"
 
 export const Route = createFileRoute("/")({
   component: Index,
 })
 
 function Index() {
-  const { data } = useQuery(listUsers)
+  const { data } = useQuery(listInstruments)
   console.log("data", data)
   return (
     <div className="p-2">
